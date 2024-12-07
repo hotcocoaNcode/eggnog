f3ac596a0d3ae50742f77c7cc0a76fe736f75499
hotcocoancode <elee9@madison.k12.wi.us>
2024-12-06 23:19:26 -0600

    update to latest JE

--- JoshEngine.js
+++ JoshEngine.js
@@ -35,7 +35,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /var/folders/zj/nqc5l1m13tn9qkpn_1dh1cjw0000gn/T/tmpm17bjkjz.js
+// include: /var/folders/zj/nqc5l1m13tn9qkpn_1dh1cjw0000gn/T/tmp4wkrf97z.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -222,21 +222,21 @@ Module['FS_createPath']("/", "textures", true, true);
 
   })();
 
-// end include: /var/folders/zj/nqc5l1m13tn9qkpn_1dh1cjw0000gn/T/tmpm17bjkjz.js
-// include: /var/folders/zj/nqc5l1m13tn9qkpn_1dh1cjw0000gn/T/tmpdl35sevx.js
+// end include: /var/folders/zj/nqc5l1m13tn9qkpn_1dh1cjw0000gn/T/tmp4wkrf97z.js
+// include: /var/folders/zj/nqc5l1m13tn9qkpn_1dh1cjw0000gn/T/tmpbjaztb13.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /var/folders/zj/nqc5l1m13tn9qkpn_1dh1cjw0000gn/T/tmpdl35sevx.js
-// include: /var/folders/zj/nqc5l1m13tn9qkpn_1dh1cjw0000gn/T/tmpmqodpjp7.js
+  // end include: /var/folders/zj/nqc5l1m13tn9qkpn_1dh1cjw0000gn/T/tmpbjaztb13.js
+// include: /var/folders/zj/nqc5l1m13tn9qkpn_1dh1cjw0000gn/T/tmppvvus8s8.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /var/folders/zj/nqc5l1m13tn9qkpn_1dh1cjw0000gn/T/tmpmqodpjp7.js
+  // end include: /var/folders/zj/nqc5l1m13tn9qkpn_1dh1cjw0000gn/T/tmppvvus8s8.js
 
 
 // Sometimes an existing Module object exists with properties
